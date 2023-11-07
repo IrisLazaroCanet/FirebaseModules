f35649a9db2910f9d2bb604f161e34dc0c824bf5
Iris LC <iris.lazaro@enti.cat>
2023-11-07 10:17:25 +0100

    Files, main activity, change theme colors

---
+++ app/src/main/java/com/enti/dostres/cdi/irislazarocanet/modulodosfirebasecdi/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.enti.dostres.cdi.irislazarocanet.modulodosfirebasecdi
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity_screen)
+    }
+}
