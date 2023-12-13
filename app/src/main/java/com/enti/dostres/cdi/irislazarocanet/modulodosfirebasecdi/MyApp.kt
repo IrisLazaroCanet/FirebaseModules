5e8a9bfad22661616813dab79eb9d9799b63f838
Iris LC <iris.lazaro@enti.cat>
2023-12-13 20:14:01 +0100

    MyApp.kt

--- app/src/main/java/com/enti/dostres/cdi/irislazarocanet/modulodosfirebasecdi/MyApp.kt
+++ app/src/main/java/com/enti/dostres/cdi/irislazarocanet/modulodosfirebasecdi/MyApp.kt
@@ -1,6 +1,10 @@
 package com.enti.dostres.cdi.irislazarocanet.modulodosfirebasecdi
 
+import android.app.Activity
 import android.app.Application
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.core.content.getSystemService
 import com.enti.dostres.cdi.irislazarocanet.modulodosfirebasecdi.firebase.FB
 
 class MyApp : Application() {
@@ -17,5 +21,11 @@ class MyApp : Application() {
         instance = this
         FB.init(this)
         FB.analytics.logOpenApp()
+    }
+
+    fun CloseKeyboard(focusedView: View)
+    {
+        val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(focusedView.windowToken, 0)
     }
 }
